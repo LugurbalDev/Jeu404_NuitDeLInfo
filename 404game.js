c1b7132fe0425c328258a1bf7b3f6ac03f10c484
eltnecniev <58566833+eltnecniev@users.noreply.github.com>
2019-12-05 21:44:44 +0100

    v3

--- 404game.js
+++ 404game.js
@@ -1,12 +1,21 @@
 var image=document.querySelector("#level1");
 var x=0;
-var 
-window.setInterval(function(){
-x+=1
-image.style.right=x+"px";},100);
+var taille=image.offsetHeight;
 
 
+console.log(taille)
+window.setInterval(deplacement,100);
 
+
+function deplacement(){
+	x-=1
+
+	image.style.left=x+"px";
+	console.log(x)
+
+
+	image.style.height = "100%";
+}
 
 	
 
