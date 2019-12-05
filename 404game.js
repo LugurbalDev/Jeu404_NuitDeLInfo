8ba7eba87af4844993cfd500964634d7b581620d
eltnecniev <58566833+eltnecniev@users.noreply.github.com>
2019-12-05 21:55:23 +0100

    Add files via upload

--- 404game.js
+++ 404game.js
@@ -3,17 +3,13 @@ var x=0;
 var taille=image.offsetHeight;
 
 
-console.log(taille)
 window.setInterval(deplacement,100);
 
 
 function deplacement(){
-	x-=1
+	x-=1;
 
 	image.style.left=x+"px";
-	console.log(x)
-
-
 	image.style.height = "100%";
 }
 
